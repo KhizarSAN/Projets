b83bc5e9f69f128922ac60fdbe690c1be40555c8
KhizarSAN <80525987+KhizarSAN@users.noreply.github.com>
2021-06-05 18:48:10 +0200

    Add files via upload

---
+++ Calculette_salariale/lecture fiche de paie automatique.py
@@ -0,0 +1,18 @@
+import openpyxl
+
+wb = openpyxl.load_workbook("C:\\Users\\konhotom\\Desktop\\exemple.xlsx")
+pg1 = wb['Fiche_de_paie']
+data1 = pg1['E12'].value
+data2 = pg1['F12'].value
+salaire_brut = data1*data2
+TAUX = int(input("A quel taux votre TVA est inscrite(1,2,3,4):"))
+if TAUX == 1 :
+    TVA = 0.2
+elif TAUX == 2 :
+    TVA = 0.1
+elif TAUX == 3 :
+    TVA = 0.055
+elif TAUX == 4 :
+    TVA = 0.021
+TVA = TVA * salaire_brut
+print("LA TVA vous retire:", TVA, "euro de votre salaire brut , votre salaire net est donc de :" ,salaire_brut - TVA , "euro")
